d27de3efed2de9cee2088058b1d6d6d06f359ad5
Jiabin Liao <benliao985@gmail.com>
2023-03-31 18:59:09 +0800

    s1-run-jobs-in-parallel
    This samle shows how to run jobs concurrently in PowerShell. Jobs will run at background.

---
+++ RegAppInParallel.ps1
@@ -0,0 +1,24 @@
+﻿#Loop through the app list
+Get-Content "AppList.txt" | %{
+
+    #Define what each job does.
+    $ScriptBlock = {
+        param($AppName)
+        if($AppName -ne $null){
+            soamview app $AppName -p | Set-Content -Force -Path M:\$AppName.xml -Verbose -Encoding ASCII
+            if($?){
+                    soamreg M:\$AppName.xml -f >> M:\RegApp.log
+                    Start-Sleep -Seconds 120
+            }else {
+                Write-Host "$AppName profile not saved. " >> M:\RegApp.log
+            }
+         }else {
+            Write-Host "$AppName has something wrong. " >> M:\RegApp.log
+        }
+
+    }
+
+    #start jobs at background in parallel.
+    Start-Job $ScriptBlock -ArgumentList $_ 
+
+}
